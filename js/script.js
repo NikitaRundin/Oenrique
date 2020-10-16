f5ea66c2a6392322e6f799835615fc0dce10ddf5
NikitaRundin <70906838+NikitaRundin@users.noreply.github.com>
2020-10-16 17:36:29 +0300

    index page

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
+            },
+
+            1000:{
+                items:1
+            }
+        }
+    });
+});
+$(document).ready(function() {
+    $("a.anchor").click(function () {
+        var elementClick = $(this).attr("href")
+        var destination = $(elementClick).offset().top;
+        jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1500);
+        return false;
+    });
+});
